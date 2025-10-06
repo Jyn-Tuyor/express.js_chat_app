be5695c89f2367e4a735a486cea4462bd67cd513
Jyn-Tuyor <tuyor.jynchris@gmail.com>
2025-10-06 21:48:59 +0000

    combined create/update profile

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -45,23 +45,28 @@ exports.updateProfile = async (req, res) => {
     try {
         const { year_level, bio, gender, hobby_1, hobby_2, not_good } = req.body;
 
-        const user = await User.findOne({ where: { id: req.session.user.id }, include: UserProfile })
-        console.log("yl", year_level)
-        user.user_profile.year_level = year_level
-        user.user_profile.bio = bio
-        user.user_profile.gender = gender
-        user.user_profile.hobby_1 = hobby_1
-        user.user_profile.hobby_2 = hobby_2
-        user.user_profile.not_good = not_good
+        const user = await User.findOne({ where: { id: req.session.user.id }, include: [{ model: UserProfile, as: "profile" }] })
 
-        await user.save()
-        await user.user_profile.save()
+        if (user.profile) {
+            user.profile.year_level = year_level
+            user.profile.bio = bio
+            user.profile.gender = gender
+            user.profile.hobby_1 = hobby_1
+            user.profile.hobby_2 = hobby_2
+            user.profile.not_good = not_good
+
+            await user.save()
+            await user.profile.save()
+        } else {
+            return this.createProfile(req, res);
+        }
+
 
         req.session.user = {
             id: user.id,
             id_number: user.id_number,
             username: user.username,
-            profile: user.user_profile || null
+            profile: user.profile || null
         }
 
         res.redirect('/users/my-profile')
@@ -75,16 +80,16 @@ exports.updateProfile = async (req, res) => {
 exports.chatRoom = async (req, res) => {
     const user = req.session.user;
 
-    const chats = await Chat.findAll({ 
-        where: { 
-            broadcast: 'global' 
-        }, 
+    const chats = await Chat.findAll({
+        where: {
+            broadcast: 'global'
+        },
         include: [
-        { 
-            model: User,
-            as: "sender" 
-        }],
-        limit: 20 
+            {
+                model: User,
+                as: "sender"
+            }],
+        limit: 20
     });
     // console.log(chats)
 
